7c53c1a1e646ae8320fdfcd70f89c39870651304
Mark AI <chanthavong_12@hotmail.com>
2021-12-23 20:19:42 +0700

    add sign in page

---
+++ sign-in.php
@@ -0,0 +1,15 @@
+<?php
+$title = 'Sign in';
+include_once 'header.php';
+?>
+
+<form action="" method="post">
+    <input type="text" name="username" placeholder="Username">
+    <input type="password" name="password" placeholder="Password">
+    <input type="submit" name="submit" value="Sign In">
+</form>   
+
+<?php
+include_once 'footer.php';
+?>
+
--- sign-up.php
+++ sign-up.php
@@ -1,13 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Sign Up</title>
-</head>
-<body>
-  <h3>Sign Up</h3>
+<?php
+$title = 'Sign up';
+include_once 'header.php';
+?>
+ 
+ <h3>Sign Up</h3>
   <input name="gender" type="radio" value="F" id="f">
   <label for="f">Female </label>
   <input name="gender" type="radio" value="M" id="m">
@@ -20,5 +16,8 @@
   <input type="text" placeholder="Phone">
   <input type="date" placeholder="Birthday">
   <textarea name="" placeholder="Address"></textarea>
-</body>
-</html>
+
+<?php
+include_once 'footer.php';
+?>
+
