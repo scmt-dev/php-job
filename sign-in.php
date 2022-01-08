2613076495da8e53385ff98bf95b167746e926b6
Mark AI <chanthavong_12@hotmail.com>
2022-01-08 08:57:35 +0700

    fix form signin: git config

--- sign-in.php
+++ sign-in.php
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-<?php include_once 'header.php';?>
-
-<div class="div-center" style="width:300px">
-<form action="" methed="post">
-  <h2>Sign In</h2>
-  <input name="username" type="text" placeholder="Username" class="w100 my-1" required>
-  <input name="password" type="password" placeholder="Username" class="w100" required>
-
-  
-
-  <button name="signin" value="signin" class="my-1">Sign in</button>
-</form>
-</div>
-
-<?php include_once 'footer.php';?>
-=======
 <?php
 $title = 'Sign in';
 include_once 'header.php';
@@ -30,4 +13,3 @@ include_once 'header.php';
 include_once 'footer.php';
 ?>
 
->>>>>>> test
